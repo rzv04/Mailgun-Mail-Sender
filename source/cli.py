e6315f05ad17857283df996f6991dd121c4eef1f
YoloTheBEst <mrs_razvan2004@yahoo.com>
2023-09-16 15:26:01 +0300

    Added initial support for crude CLI main mailgun.py
    Added initial support for crude CLI main mailgun.py

--- source/cli.py
+++ source/cli.py
@@ -7,61 +7,72 @@ from mailgun import Mail
 import time
 
 
-mail=Mail()
+mail = Mail()
+
 
 class MainMenu:
- 
     def load_contents_and_send_email(self) -> None:
-
         mail.set_mail_contents()
         mail.send_email()
 
     def set_params_interactive_feedback(self) -> None:
         mail.set_params()
-        self.data_loaded = consolemenu.Screen().println(colorama.Fore.GREEN+"Data loaded successfully")
+        self.data_loaded = consolemenu.Screen().println(
+            colorama.Fore.GREEN + "Data loaded successfully"
+        )
         time.sleep(1)
 
     def set_params_csv_feedback(self) -> None:
         mail.set_params_from_csv()
-        self.data_loaded = consolemenu.Screen().println(colorama.Fore.GREEN+"Data loaded successfully")
+        self.data_loaded = consolemenu.Screen().println(
+            colorama.Fore.GREEN + "Data loaded successfully"
+        )
         time.sleep(1)
 
-
     def __init__(self) -> None:
-        
+        self.hero_title = pyfiglet.figlet_format("Mail-Sender", "small")
 
-        self.hero_title = pyfiglet.figlet_format("Mail-Sender", 'small')
-        
-        self.hero_subtitle ="Send emails using the Mailgun API"
+        self.hero_subtitle = "Send emails using the Mailgun API"
 
-        self.menu=consolemenu.ConsoleMenu(colorama.Fore.GREEN+self.hero_title,subtitle=colorama.Fore.BLUE+self.hero_subtitle, show_exit_option=False)
-        
-        self.enter_api_key_interactive = consolemenu.items.FunctionItem(colorama.Fore.GREEN+"Input API credentials", self.set_params_interactive_feedback)
-        self.enter_api_key_csv = consolemenu.items.FunctionItem(colorama.Fore.GREEN+"Input API credentials from api.csv", self.set_params_csv_feedback) ##
-        
-        self.send_email_prompt = consolemenu.items.FunctionItem(colorama.Fore.GREEN+"Send An Email", self.load_contents_and_send_email) ##
-        self.check_mail_prompt = consolemenu.items.FunctionItem(colorama.Fore.GREEN+"Check Recieved Messages (Not Implemented)", mail.check_mails) #TODO
+        self.menu = consolemenu.ConsoleMenu(
+            colorama.Fore.GREEN + self.hero_title,
+            subtitle=colorama.Fore.BLUE + self.hero_subtitle,
+            show_exit_option=False,
+        )
 
-        self.exit_menu = consolemenu.items.ExitItem(colorama.Fore.RED+"Exit")
-       
+        self.enter_api_key_interactive = consolemenu.items.FunctionItem(
+            colorama.Fore.GREEN + "Input API credentials",
+            self.set_params_interactive_feedback,
+        )
+        self.enter_api_key_csv = consolemenu.items.FunctionItem(
+            colorama.Fore.GREEN + "Input API credentials from api.csv",
+            self.set_params_csv_feedback,
+        )  ##
+
+        self.send_email_prompt = consolemenu.items.FunctionItem(
+            colorama.Fore.GREEN + "Send An Email", self.load_contents_and_send_email
+        )  ##
+        self.check_mail_prompt = consolemenu.items.FunctionItem(
+            colorama.Fore.GREEN + "Check Recieved Messages (Not Implemented)",
+            mail.check_mails,
+        )  # TODO
+
+        self.exit_menu = consolemenu.items.ExitItem(colorama.Fore.RED + "Exit")
+
         self.menu.append_item(self.enter_api_key_interactive)
-        self.menu.append_item(self.enter_api_key_csv )
+        self.menu.append_item(self.enter_api_key_csv)
         self.menu.append_item(self.send_email_prompt)
         self.menu.append_item(self.check_mail_prompt)
         self.menu.append_item(self.exit_menu)
-        
-        
+
     def show(self) -> None:
         self.menu.show()
 
 
 def main():
-
     console = MainMenu()
     console.show()
 
-    
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
--- source/mailgun.py
+++ source/mailgun.py
@@ -3,12 +3,12 @@ Simple mail-sending driving code using MailGun.
 The authentication is insecure, and email validation is not yet implemented.
 """
 
-#TODO:
+# TODO:
 # -Authentication from CSV file- complete
-#Linux-like command-line launch(with argument parsing)
+# Linux-like command-line launch(with argument parsing)
 import os
 import argparse
-from abc import abstractclassmethod,abstractmethod
+from abc import abstractclassmethod, abstractmethod
 import requests
 from sys import exit
 import csv
@@ -17,59 +17,61 @@ from typing import List
 
 class MailGun:
 
-
     """
     Parent MailGun Class. Contains methods for obtaining the API Key, mail address,etc.
 
     """
-    
 
-    def __init__(self,api_key="",domain_name="",api_url="",domain_country="") -> None:
+    def __init__(
+        self, api_key="", domain_name="", api_url="", domain_country="", csv_path=""
+    ) -> None:
+        self.api_key = api_key
+        self.domain_name = domain_name
+        self.api_url = api_url
+        self.domain_country = domain_country
+        self.csv_path = csv_path
 
-        self.api_key=api_key
-        self.domain_name=domain_name
-        self.api_url=api_url
-        self.domain_country=domain_country
-
-    def set_params(self) -> 'Mail':
-
-        self.api_key=input("Enter API key:\n")
-        self.domain_country=input("Enter domain country:\n").strip().upper()
-        self.domain_name=input("Enter domain name:\n")
+    def set_params(self):
+        self.api_key = input("Enter API key:\n")
+        self.domain_country = input("Enter domain country:\n").strip().upper()
+        self.domain_name = input("Enter domain name:\n")
 
         match self.domain_country:
-
             case "US":
-                self.api_url="https://api.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
+                self.api_url = (
+                    "https://api.mailgun.net/v3/{domain_name}/messages".format(
+                        domain_name=self.domain_name
+                    )
+                )
             case "EU":
-                self.api_url="https://api.eu.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
+                self.api_url = (
+                    "https://api.eu.mailgun.net/v3/{domain_name}/messages".format(
+                        domain_name=self.domain_name
+                    )
+                )
             case _:
-                self.api_url="https://api.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
+                self.api_url = (
+                    "https://api.mailgun.net/v3/{domain_name}/messages".format(
+                        domain_name=self.domain_name
+                    )
+                )
 
-            
         return self
 
-
-        
-
-    #Not implemented correctly
+    # Not implemented correctly
     @abstractclassmethod
-    def check_api_key(cls,api_key,api_url) -> None:
-        authenticate=requests.post(
-        api_url,
-        auth=('api',api_key)
-        )
+    def check_api_key(cls, api_key, api_url):
+        authenticate = requests.post(api_url, auth=("api", api_key))
         if authenticate.status_code == 200:
             print("Authentication successful")
 
         else:
-            print("Error:",authenticate.text)
+            print("Error:", authenticate.text)
             exit(1)
 
-    
     @abstractmethod
-    def send_email(cls): ...
-
+    def send_email(cls):
+        ...
 
     def set_params_from_csv(self) -> None:
         """
@@ -77,38 +79,56 @@ class MailGun:
         The CSV file should be named "api.csv" and formatted thusly:
         api_key,domain_country,domain_name
         """
-        with open ("api.csv","r") as read_file:
-            reader=csv.DictReader(read_file)
+
+        with open(self.csv_path, "r") as read_file:
+            reader = csv.DictReader(read_file)
 
             for i in reader:
                 try:
-                    self.api_key=i['api_key']
-                    self.domain_country=i['domain_country'].upper()
-                    self.domain_name=i['domain_name']
-                    
+                    self.api_key = i["api_key"]
+                    self.domain_country = i["domain_country"].upper()
+                    self.domain_name = i["domain_name"]
+
                 except KeyError:
                     print("CSV File is incorrectly formatted.")
                     exit(1)
-            
-            
-            match self.domain_country:
 
+            match self.domain_country:
                 case "US":
-                    self.api_url="https://api.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
+                    self.api_url = (
+                        "https://api.mailgun.net/v3/{domain_name}/messages".format(
+                            domain_name=self.domain_name
+                        )
+                    )
                 case "EU":
-                    self.api_url="https://api.eu.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
+                    self.api_url = (
+                        "https://api.eu.mailgun.net/v3/{domain_name}/messages".format(
+                            domain_name=self.domain_name
+                        )
+                    )
                 case _:
-                    self.api_url="https://api.mailgun.net/v3/{domain_name}/messages".format(domain_name=self.domain_name)
-                
-        #Getters
+                    self.api_url = (
+                        "https://api.mailgun.net/v3/{domain_name}/messages".format(
+                            domain_name=self.domain_name
+                        )
+                    )
+
+        # Getters
+
     def get_api_key(self) -> str:
         return self.api_key
-    
-        #Particular Setter for Parser - experimental
-    def set_api_key(self,api_key) -> str:
-        self.api_key=api_key
-    
 
+    def get_csv_path(self) -> str:
+        return self.csv_path
+
+        # Particular Setter for Parser - experimental
+
+    def set_csv_path(self, csv_path):
+        if os.path.exists(csv_path):
+            self.csv_path = csv_path
+        else:
+            raise FileNotFoundError ("Incorrect CSV file path.")
+            
 
 
 class Mail(MailGun):
@@ -118,39 +138,37 @@ class Mail(MailGun):
     """
 
     def set_mail_contents(self):
-        self.from_name=input("Input your name: ")
-        self.to_emails=input("Input emails to send to: ").split()
-        self.subject=input("Enter email Title/Subject: ")
-        self.content=input("Enter email content:\n")
-        
+        self.from_name = input("Input your name: ")
+        self.to_emails = input("Input emails to send to: ").split()
+        self.subject = input("Enter email Title/Subject: ")
+        self.content = input("Enter email content:\n")
+
         return self
 
-
-    def send_email(self) -> 'Mail':
-
+    def send_email(self) -> "Mail":
         try:
             request = requests.post(
-                    self.api_url,
-                    auth=('api', self.api_key),
-                    data={
-                        'from': f'{self.from_name} <mailgun@{self.domain_name}>',
-                        'to': self.to_emails,
-                        'subject': self.subject,
-                        'text': self.content
-                    }
-                )
+                self.api_url,
+                auth=("api", self.api_key),
+                data={
+                    "from": f"{self.from_name} <mailgun@{self.domain_name}>",
+                    "to": self.to_emails,
+                    "subject": self.subject,
+                    "text": self.content,
+                },
+            )
         except Exception:
             print("Incorrect request format.")
+            exit(1)
 
         if request.status_code == 200:
             print("Mail sent successfully!")
 
         else:
-            print("Error:",request.text)
+            print("Error:", request.text)
             exit(1)
 
         return self
-
 
     def check_mails(self):
         pass
@@ -158,37 +176,48 @@ class Mail(MailGun):
     def get_to_emails(self) -> List[str]:
         return self.to_emails
 
-def Parser(mail):
 
-    #parser=None
+def Parser(mail):
+    # parser=None
     def init_parser(mail):
-        parser=argparse.ArgumentParser(description="Send emails using MailGun API.")
-        # parser.add_argument('-k','--key',type=str,nargs='?',help='MailGun API Key',required=False)
-        # parser.add_argument('-d','--domain-name',nargs='?',help='Set domain name (email address)',required=False)
-        # parser.add_argument('-c','--domain-country',nargs='?',help='Set domain country (EU or US)',required=False)
-        parser.add_argument('--api-key',required=True,nargs='?',
-                            help="Set MailGun API key",type=str,action='store',
-                            const=mail.set_api_key)
-                            
-        parser.parse_args()
+        parser = argparse.ArgumentParser(description="Send emails using MailGun API.")
+
+        # parser.add_argument(
+        #     "--api-key",
+        #     required=False,
+        #     nargs="?",
+        #     help="Set MailGun API key",
+        #     type=str,
+        #     action="store",
+        #     const=mail.set_api_key,
+        # )
+
+        parser.add_argument(
+            "--csv",
+            required=True,
+            nargs="?",
+            help="""Path to CSV file with the following:
+            api_key,domain_country,domain_name""",
+            action="store",
+        )
+
+        args = parser.parse_args()
+        mail.set_csv_path(args.csv)
+        mail.set_params_from_csv()
+
     return init_parser
 
 
-#Example flow
+# Example flow
 def main():
-
-    mail=Mail()
+    mail = Mail()
     # mail.set_params_from_csv()
     # mail.set_mail_contents()
     # mail.send_email()
 
-    parse_init = Parser(mail) #Closure
+    parse_init = Parser(mail)  # Closure
     parser = parse_init(mail)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
